1ff373a8df444537869e2095efe454645da1e8b6
Afina2005 <noorilafina7@gmail.com>
2026-03-23 20:48:09 +0530

    Update dashboardConfig.js

--- backend/models/dashboardConfig.js
+++ backend/models/dashboardConfig.js
@@ -1,1 +1,9 @@
+const mongoose = require("mongoose");
 
+const dashboardConfigSchema = new mongoose.Schema({
+  userId: { type: String, required: true },
+  company: { type: String, default: "Halleyx" },
+  layout: { type: Array, default: [] }
+}, { timestamps: true });
+
+module.exports = mongoose.model("DashboardConfig", dashboardConfigSchema);
